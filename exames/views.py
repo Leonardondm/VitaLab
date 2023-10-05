32180c92c02003d0e3a571a149e8dc541accd42e
Leonardondm <leonardondm@hotmail.com>
2023-10-04 23:33:58 -0300

    app exames

--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import TiposExames, PedidosExames, SolicitacaoExame
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.messages import constants
+import locale
 
-# Create your views here.
+# Defina o idioma local para o português do Brasil ('pt_BR')
+locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
+
+
+@login_required
+def solicitar_exames(request):
+    tipos_exames = TiposExames.objects.all()
+    data_atual = datetime.now().strftime('%d de %B')
+    if request.method == "GET":
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
+    else:
+        exames_id = request.POST.getlist('exames')
+        solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+       
+        #preco_total = solicitacao_exames.aggregate(total=Sum('preco'))['total']
+       
+        preco_total = 0
+        for i in solicitacao_exames:
+            if i.disponivel:
+                preco_total += i.preco
+        
+        return render(request, 'solicitar_exames.html', {'solicitacao_exames': solicitacao_exames, 'preco_total': preco_total, 'tipos_exames': tipos_exames, 'data_atual': data_atual})
+
+
+@login_required
+def fechar_pedido(request):
+    exames_id = request.POST.getlist('exames')
+    solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+
+    pedido_exame = PedidosExames(
+        usuario = request.user,
+        data = datetime.now().strftime('%Y-%m-%d')
+    )
+
+    pedido_exame.save()
+
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp = SolicitacaoExame(
+            usuario=request.user,
+            exame=exame,
+            status="E"
+        )
+        solicitacao_exames_temp.save()
+        pedido_exame.exames.add(solicitacao_exames_temp)
+        
+    pedido_exame.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Pedido de exame concluído com sucesso')
+    return redirect('/exames/ver_pedidos/')
+    
